4c0315af8707705102f431e5ad5ab68d89c525a6
elianCoMa <ecordovam@isise.edu.pe>
2023-04-09 11:15:36 -0500

    CREACION Y PROPERTIES

---
+++ webOnpe/src/main/java/com/onpe/WebOnpeApplication.java
@@ -0,0 +1,13 @@
+package com.onpe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebOnpeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebOnpeApplication.class, args);
+	}
+
+}
---
+++ webOnpe/src/test/java/com/onpe/WebOnpeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onpe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebOnpeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
